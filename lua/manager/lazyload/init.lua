895d3adde93430c3903ddd0f66c5dd0d597dfe78
shizukani-cp <shizukani.cp@gmail.com>
2026-02-28 17:59:26 +0900

    refactor!: change to setup-based extension for Manager instance

--- lua/manager/lazyload/init.lua
+++ lua/manager/lazyload/init.lua
@@ -1,33 +1,31 @@
-local M = {}
-
-local manager = require("manager.core")
-
-function M.key(mode, lhs, rhs, plugin_id, opts)
-    vim.keymap.set(
-        mode, lhs,
-        function()
-            manager.load(plugin_id)
-            vim.keymap.set(mode, lhs, rhs, opts)
-            if type(rhs) == "string" then
-                local keys = vim.api.nvim_replace_termcodes(rhs, true, false, true)
-                vim.api.nvim_feedkeys(keys, 'n', true)
-            else
-                rhs()
-            end
-        end,
-        opts
-    )
+local function key(self, mode, lhs, rhs, plugin_id, opts)
+	vim.keymap.set(mode, lhs, function()
+		self:load(plugin_id)
+		vim.keymap.set(mode, lhs, rhs, opts)
+		if type(rhs) == "string" then
+			local keys = vim.api.nvim_replace_termcodes(rhs, true, false, true)
+			vim.api.nvim_feedkeys(keys, "n", true)
+		else
+			rhs()
+		end
+	end, opts)
 end
 
-function M.event(event_name, plugin_id, pattern)
-    vim.api.nvim_create_autocmd(event_name, {
-        group = vim.api.nvim_create_augroup(plugin_id .. "_load", { clear = true }),
-        pattern = pattern or "*",
-        callback = function()
-            manager.load(plugin_id)
-        end
-    })
-    return M
+local function event(self, event_name, plugin_id, pattern)
+	vim.api.nvim_create_autocmd(event_name, {
+		group = vim.api.nvim_create_augroup(plugin_id .. "_load", { clear = true }),
+		pattern = pattern or "*",
+		callback = function()
+			self:load(plugin_id)
+		end,
+	})
+end
+
+local M = {}
+
+function M.setup(manager)
+	manager.lazyload_key = key
+	manager.lazyload_event = event
 end
 
 return M
