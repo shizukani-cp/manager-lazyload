2dbd204be056a82150b364d1f85eac1a14b212d7
shizukani-cp <shizukani.cp@gmail.com>
2026-03-01 16:21:36 +0900

    chore: add type definitions

--- lua/manager/lazyload/init.lua
+++ lua/manager/lazyload/init.lua
@@ -1,3 +1,11 @@
+---@alias Manager.LazyloadKey fun(self: Manager, mode: string|string[], lhs: string, rhs: string|function, plugin_id: string, opts?: table)
+---@alias Manager.LazyloadEvent fun(self: Manager, event_name: string|string[], plugin_id: string, pattern?: string|string[])
+
+---@class Manager
+---@field lazyload_key Manager.LazyloadKey
+---@field lazyload_event Manager.LazyloadEvent
+
+---@type Manager.LazyloadKey
 local function key(self, mode, lhs, rhs, plugin_id, opts)
 	vim.keymap.set(mode, lhs, function()
 		self:load(plugin_id)
@@ -11,6 +19,7 @@ local function key(self, mode, lhs, rhs, plugin_id, opts)
 	end, opts)
 end
 
+---@type Manager.LazyloadEvent
 local function event(self, event_name, plugin_id, pattern)
 	vim.api.nvim_create_autocmd(event_name, {
 		group = vim.api.nvim_create_augroup(plugin_id .. "_load", { clear = true }),
@@ -24,6 +33,7 @@ end
 
 local M = {}
 
+---@param manager Manager
 function M.setup(manager)
 	manager.lazyload_key = key
 	manager.lazyload_event = event
