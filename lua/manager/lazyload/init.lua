7ea8d5fad24ae0ac6c10d5a94ca1ea9fabcc7f7e
shizukani-cp <shizukani.cp@gmail.com>
2026-03-08 05:39:13 +0900

    feat: add support for command-based lazy loading

--- lua/manager/lazyload/init.lua
+++ lua/manager/lazyload/init.lua
@@ -1,11 +1,13 @@
 ---@alias Manager.LazyloadKey fun(self: Manager, mode: string|string[], lhs: string, rhs: string|function, plugin_id: string, opts?: table)
 ---@alias Manager.LazyloadEvent fun(self: Manager, event_name: string|string[], plugin_id: string, pattern?: string|string[])
 ---@alias Manager.LazyloadFileType fun(self: Manager, filetype: string|string[], plugin_id: string)
+---@alias Manager.LazyloadCommand fun(self: Manager, command: string|string[], plugin_id: string)
 
 ---@class Manager
 ---@field lazyload_key Manager.LazyloadKey
 ---@field lazyload_event Manager.LazyloadEvent
 ---@field lazyload_filetype Manager.LazyloadFileType
+---@field lazyload_command Manager.LazyloadCommand
 
 ---@type Manager.LazyloadKey
 local function key(self, mode, lhs, rhs, plugin_id, opts)
@@ -44,6 +46,27 @@ local function filetype(self, filetype, plugin_id)
     })
 end
 
+---@type fun(self: Manager, cmd: string, plugin_id: string)
+local function create_cmd(self, cmd, plugin_id)
+    vim.api.nvim_create_user_command(cmd, function(opts)
+        vim.api.nvim_del_user_command(cmd)
+        self:load(plugin_id)
+        local command_str = string.format("%s%s %s", opts.bang and "!" or "", cmd, opts.args or "")
+        vim.cmd(command_str)
+    end, { bang = true, nargs = "*" })
+end
+
+---@type Manager.LazyloadCommand
+local function command(self, cmd, plugin_id)
+    if type(cmd) == "string" then
+        create_cmd(self, cmd, plugin_id)
+    elseif type(cmd) == "table" then
+        for _, c in ipairs(cmd) do
+            create_cmd(self, c, plugin_id)
+        end
+    end
+end
+
 local M = {}
 
 ---@param manager Manager
@@ -51,6 +74,7 @@ function M.setup(manager)
     manager.lazyload_key = key
     manager.lazyload_event = event
     manager.lazyload_filetype = filetype
+    manager.lazyload_command = command
 end
 
 return M
