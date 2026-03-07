e2ebcea24aa36127f23cd2d5d5f1d5f72f334027
shizukani-cp <shizukani.cp@gmail.com>
2026-03-07 18:44:44 +0900

    style: apply stylua formatting

--- lua/manager/lazyload/init.lua
+++ lua/manager/lazyload/init.lua
@@ -7,36 +7,36 @@
 
 ---@type Manager.LazyloadKey
 local function key(self, mode, lhs, rhs, plugin_id, opts)
-	vim.keymap.set(mode, lhs, function()
-		self:load(plugin_id)
-		vim.keymap.set(mode, lhs, rhs, opts)
-		if type(rhs) == "string" then
-			local keys = vim.api.nvim_replace_termcodes(rhs, true, false, true)
-			vim.api.nvim_feedkeys(keys, "n", true)
-		else
-			rhs()
-		end
-	end, opts)
+    vim.keymap.set(mode, lhs, function()
+        self:load(plugin_id)
+        vim.keymap.set(mode, lhs, rhs, opts)
+        if type(rhs) == "string" then
+            local keys = vim.api.nvim_replace_termcodes(rhs, true, false, true)
+            vim.api.nvim_feedkeys(keys, "n", true)
+        else
+            rhs()
+        end
+    end, opts)
 end
 
 ---@type Manager.LazyloadEvent
 local function event(self, event_name, plugin_id, pattern)
-	vim.api.nvim_create_autocmd(event_name, {
-		group = vim.api.nvim_create_augroup(plugin_id .. "_load", { clear = true }),
-		pattern = pattern or "*",
-		once = true,
-		callback = function()
-			self:load(plugin_id)
-		end,
-	})
+    vim.api.nvim_create_autocmd(event_name, {
+        group = vim.api.nvim_create_augroup(plugin_id .. "_load", { clear = true }),
+        pattern = pattern or "*",
+        once = true,
+        callback = function()
+            self:load(plugin_id)
+        end,
+    })
 end
 
 local M = {}
 
 ---@param manager Manager
 function M.setup(manager)
-	manager.lazyload_key = key
-	manager.lazyload_event = event
+    manager.lazyload_key = key
+    manager.lazyload_event = event
 end
 
 return M
